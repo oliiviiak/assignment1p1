e34672670427e98b142dc07178108f4c42683afe
oliiviiak <242837704+oliiviiak@users.noreply.github.com>
2026-01-13 14:22:12 -0800

    made function skeletons

---
+++ assignment1p1/a1p1.py
@@ -0,0 +1,20 @@
+# Olivia Kong
+# okyong@uci.edu
+# 92692989
+
+def list_files():
+
+def list_recursively():
+
+def list_only_files():
+
+def list_exact_files():
+
+def list_files_extensions():
+
+
+def main():
+    command, path = input().split(" ")
+
+if __name__ == "__main__":
+    main()
