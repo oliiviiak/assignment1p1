6f8683b50f310e2652c701208174246a96197c83
oliiviiak <242837704+oliiviiak@users.noreply.github.com>
2026-01-13 14:53:49 -0800

    made the list_files method list the files before the dirs

--- assignment1p1/a1p1.py
+++ assignment1p1/a1p1.py
@@ -7,13 +7,17 @@ def print_values(list_name):
 
 def list_files(path):
     p = Path(path)
-    all_files_dirs = []
+    all_files = []
+    all_dirs = []
 
     for item in p.iterdir():
-        all_files_dirs.append(item)
+        if item.is_file():
+            all_files.append(item)
+        else:
+            all_dirs.append(item)
     
-    print_values(all_files_dirs)
-
+    print_values(all_files)
+    print_values(all_dirs)
 
 def list_recursively(path):
     p = Path(path)
@@ -33,6 +37,9 @@ def list_files_extensions(path, extension_name):"""
 
 def main():
     #command, path = input().split(" ")
+    print ("list (not recursively)")
+    list_files("testdir")
+    print ("\nlist recursively")
     list_recursively("testdir")
 
 
